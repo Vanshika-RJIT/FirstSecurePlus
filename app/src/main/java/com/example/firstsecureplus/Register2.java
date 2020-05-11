77f76f5453ba9ecb7ca9b212200bb503e80371a6
Vanshika-RJIT <vanshikasikarwar79@gmail.com>
2020-05-11 11:18:17 +0530

    Second Commit

--- app/src/main/java/com/example/firstsecureplus/Register2.java
+++ app/src/main/java/com/example/firstsecureplus/Register2.java
@@ -2,13 +2,24 @@ package com.example.firstsecureplus;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Register2 extends AppCompatActivity {
-
+   Button b1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register2);
+        b1=(Button)findViewById(R.id.button5);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(Register2.this,MobileAuth.class);
+                startActivity(i);
+            }
+        });
     }
 }
