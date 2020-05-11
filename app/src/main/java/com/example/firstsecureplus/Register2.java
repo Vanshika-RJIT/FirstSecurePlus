f09d8985b7d0d900d74bb684f54618825bce8829
Vanshika-RJIT <vanshikasikarwar79@gmail.com>
2020-05-12 01:33:15 +0530

    Third Commit

--- app/src/main/java/com/example/firstsecureplus/Register2.java
+++ app/src/main/java/com/example/firstsecureplus/Register2.java
@@ -2,24 +2,50 @@ package com.example.firstsecureplus;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class Register2 extends AppCompatActivity {
-   Button b1;
+    Button b1;
+    ImageView t1;
+    Button t2;
+    private static int Camera_Request = 1888;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register2);
-        b1=(Button)findViewById(R.id.button5);
+        b1 = (Button) findViewById(R.id.button5);
+        ImageView t1 = (ImageView) this.findViewById(R.id.imageView3);
+        Button t2 = (Button) this.findViewById(R.id.button);
         b1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i=new Intent(Register2.this,MobileAuth.class);
+                Intent i = new Intent(Register2.this, MobileAuth.class);
                 startActivity(i);
             }
         });
+        t2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(cameraIntent, Camera_Request);
+            }
+
+        });
+
+    }
+
+    @SuppressLint("MissingSuperCall")
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == Camera_Request && resultCode == RESULT_OK) {
+            Bitmap photo = (Bitmap) data.getExtras().get("data");
+            t1.setImageBitmap(photo);
+        }
     }
 }
